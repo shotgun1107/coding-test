7df48ca6283d3dc586280d982716b5b43b731d07
shotgun1107 <129044943+shotgun1107@users.noreply.github.com>
2023-08-06 23:55:47 +0900

    [Bronze II] Title: 시험 감독, Time: 644 ms, Memory: 150704 KB -BaekjoonHub

---
+++ 백준/Bronze/13458. 시험 감독/시험 감독.py
@@ -0,0 +1,14 @@
+n = int(input())
+tester = list(map(int, input().split()))
+a, b = map(int, input().split())
+
+cnt = n
+for i in tester:
+    i -= a
+    if i > 0:
+        if i % b:
+            cnt += (i // b) + 1
+        else:
+            cnt += (i // b)
+
+print(cnt)
