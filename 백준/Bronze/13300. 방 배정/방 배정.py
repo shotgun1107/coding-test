094e77b638399a6f40ee434e5cd25395959478c4
shotgun1107 <129044943+shotgun1107@users.noreply.github.com>
2023-10-12 12:46:16 +0900

    [Bronze II] Title: 방 배정, Time: 40 ms, Memory: 33240 KB, Score: 2 point -BaekjoonHub

---
+++ 백준/Bronze/13300. 방 배정/방 배정.py
@@ -0,0 +1,12 @@
+import math 
+
+n, k = map(int,input().split())
+ls = [ tuple(map(int,input().split())) for _ in range(n)]
+man = [i[1] for i in sorted(list(filter(lambda x : x[0] == 1 , ls)),key= lambda x : x[1])]
+woman = [i[1] for i in sorted(list(filter(lambda x : x[0] == 0 , ls)),key= lambda x : x[1])]
+
+n = 0
+for i in range(max(max(woman),max(man))+1):
+    n += math.ceil(man.count(i) / k)
+    n += math.ceil(woman.count(i) / k)
+print(n)
