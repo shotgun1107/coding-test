0628a2d145c2fe9193e47dc8b3a14c62aa9ce2b1
shotgun1107 <129044943+shotgun1107@users.noreply.github.com>
2023-10-12 13:17:27 +0900

    [Bronze II] Title: 방 배정, Time: 80 ms, Memory: 33372 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/13300. 방 배정/방 배정.py
+++ 백준/Bronze/13300. 방 배정/방 배정.py
@@ -6,7 +6,7 @@ man = [i[1] for i in sorted(list(filter(lambda x : x[0] == 1 , ls)),key= lambda 
 woman = [i[1] for i in sorted(list(filter(lambda x : x[0] == 0 , ls)),key= lambda x : x[1])]
 
 n = 0
-for i in range(max(max(woman),max(man))+1):
+for i in range(max(max(woman,default= -1),max(man,default= - 1))+1):
     n += math.ceil(man.count(i) / k)
     n += math.ceil(woman.count(i) / k)
 print(n)
