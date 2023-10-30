5012eeb0dda07bfde74bac0cff7e0a792c0beecd
shotgun1107 <129044943+shotgun1107@users.noreply.github.com>
2023-10-30 15:27:42 +0900

    [Platinum V] Title: 큰 수 만들기, Time: 80 ms, Memory: 34048 KB -BaekjoonHub

---
+++ 백준/Platinum/16496. 큰 수 만들기/큰 수 만들기.py
@@ -0,0 +1,17 @@
+from functools import cmp_to_key
+
+def custom_compare(x, y):
+    if str(x) + str(y) > str(y) + str(x):
+        return 1
+    elif str(x) + str(y) < str(y) + str(x):
+        return -1
+    return 0
+
+input()
+ls = list(input().split())
+if sum([int(i) for i in ls]) == 0:
+    print(0)
+
+else: 
+    ls = sorted(ls, key=cmp_to_key(custom_compare), reverse=True)
+    print(''.join(ls))
