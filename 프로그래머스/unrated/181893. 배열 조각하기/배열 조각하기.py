28b4747c778991c31d2e2fd36acec2c0a40a3ea8
shotgun1107 <129044943+shotgun1107@users.noreply.github.com>
2023-11-01 17:38:58 +0900

    [level unrated] Title: 배열 조각하기, Time: 0.12 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181893. 배열 조각하기/배열 조각하기.py
+++ 프로그래머스/unrated/181893. 배열 조각하기/배열 조각하기.py
@@ -1,2 +1,2 @@
 def solution(arr, query):
-    return [i for i in ls[0]] if (ls := [[arr := arr[:y+1]] if x % 2 else [arr := arr[y:]]  for x,y in enumerate(query,1)][-1]) else 0
+    return [arr := arr[:y+1] if x % 2 else (arr := arr[y:])  for x,y in enumerate(query,1)][-1]
