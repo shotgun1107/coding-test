24d307d06d4985464c4098646f21aeee942a790e
shotgun1107 <129044943+shotgun1107@users.noreply.github.com>
2023-11-10 11:17:03 +0900

    [Bronze III] Title: 크림빵, Time: 44 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/28214. 크림빵/크림빵.py
+++ 백준/Bronze/28214. 크림빵/크림빵.py
@@ -1,4 +1,4 @@
 n,k,p = map(int,input().split())
 ls = list(map(int,input().split()))
-ls1 = [sum(ls[k*(i-1):k*i]) for i in range(1,n+1)]
-print(sum([1 if i >= p else 0 for i in ls1]))
+ls1 = [ls[k*(i-1):k*i] for i in range(1,n+1)]
+print(sum([1 if i.count(0) < p else 0 for i in ls1]))
