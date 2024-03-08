8ce1d4099da62cba4bd564f221e6fef1fe8a9fc0
shotgun1107 <129044943+shotgun1107@users.noreply.github.com>
2024-03-08 14:09:07 +0900

    [Silver V] Title: 2차원 배열의 합, Time: 1960 ms, Memory: 37028 KB -BaekjoonHub

--- 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.py
+++ 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.py
@@ -1,9 +1,12 @@
+from itertools import accumulate
+
 N,M = map(int,input().split())
-ls = [ [*map(int,input().split())] for _ in range(N)]
+tv = [ [*map(int,input().split())] for _ in range(N)]
+ls = [[0,*accumulate(tv[i]) ] for i in range(N)]
 
 for _ in range(int(input())):
     x1,y1,x2,y2 = map(int,input().split())
     answer = 0
     for i in range(x2-x1+1):
-        answer += sum(n := ls[x1+i-1][y1-1:y2])
+        answer += (ls[x1+i-1][y2] - ls[x1+i-1][y1-1])
     print(answer)
