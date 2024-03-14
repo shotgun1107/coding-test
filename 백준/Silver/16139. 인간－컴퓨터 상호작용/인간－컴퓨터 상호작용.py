1fd43c50fbde033715e7b0d011c6cd1b90d25994
shotgun1107 <129044943+shotgun1107@users.noreply.github.com>
2024-03-14 11:21:40 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 624 ms, Memory: 216200 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -1,8 +1,6 @@
 import sys
-from collections import deque
 
 input = sys.stdin.readline
-answer = []
 
 word = input().rstrip()
 ls = [[] for _ in range(26)]
@@ -19,13 +17,8 @@ for i,s in enumerate(word[1:]):
         if not len(ls[j]) == i+2:
             ls[j].append(ls[j][i]+0)
 
-for i in range(26):
-    ls[i] = ls[i][::-1]
-    ls[i].append(0)
-    ls[i] = ls[i][::-1]
-
 for _ in range(int(input())):
     n , x1 ,x2 = input().split()
     n , x1 , x2 = ord(n) - 97 , int(x1) , int(x2)
-    answer.append(ls[n][x2+1] - ls[n][x1])
-print(*answer,sep='\n')
+    print(ls[n][x2] - ls[n][x1-1 if not x1 - 1 <= 0 else 0] if x1 != 0 else ls[n][x2])
+
