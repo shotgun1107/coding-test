048bb3f9e6e04a3c21385649705a4db84134cddf
shotgun1107 <129044943+shotgun1107@users.noreply.github.com>
2024-04-22 19:26:13 +0900

    [Bronze IV] Title: 빵, Time: 48 ms, Memory: 31252 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/25377. 빵/빵.py
+++ 백준/Bronze/25377. 빵/빵.py
@@ -1,2 +1,2 @@
-ls = sorted(list(filter(lambda x : x[1] >= x[0] ,[ [*map(int,input().split())]  for _ in range(int(input()))])),key= lambda x : x[1] - x[0])
+ls = sorted(list(filter(lambda x : x[1] >= x[0] ,[ [*map(int,input().split())]  for _ in range(int(input()))])),key= lambda x : (x[1],x[1] - x[0]))
 print(ls[0][1] if ls else -1)
