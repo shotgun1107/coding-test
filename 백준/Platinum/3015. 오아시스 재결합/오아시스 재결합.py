f964ae95de3cca5707cb4efaabb122f22b2166a4
shotgun1107 <129044943+shotgun1107@users.noreply.github.com>
2025-02-27 01:06:50 +0900

    [Platinum V] Title: 오아시스 재결합, Time: 464 ms, Memory: 82292 KB -BaekjoonHub

--- 백준/Platinum/3015. 오아시스 재결합/오아시스 재결합.py
+++ 백준/Platinum/3015. 오아시스 재결합/오아시스 재결합.py
@@ -2,31 +2,22 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-ls = [int(input()) for _ in range(n)]
-
 stack = []
 answer = 0
 
-for i in ls:
+for _ in range(n):
+    h = int(input())
     cnt = 1
-    if not stack:
-        stack.append((i,cnt))
-        continue
-    for s in stack[::-1]:
-        if s[0] > i:
-            answer += 1
-            break
-        answer += s[1]
-    while stack:
-        s = stack.pop()
-        if s[0] >= i:
-            stack.append(s)
-            break
-    if stack and stack[-1][0] == i:
-        cnt += stack.pop()[1]
-        stack.append((i,cnt))
-    else:
-        stack.append((i,cnt))
+    
+    while stack and stack[-1][0] <= h:
+        answer += stack[-1][1]
+        if stack[-1][0] == h:
+            cnt += stack[-1][1]
+        stack.pop()
+    
+    if stack:
+        answer += 1
+    
+    stack.append((h, cnt))
+
 print(answer)
-# ls = "7 7 8 6 5 3 7 4 7 7 10 6 1 2".replace(' ','\n')
-# print(ls)
