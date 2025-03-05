326a5277db7b840d75c42bb3b98503d538bc8bb6
shotgun1107 <129044943+shotgun1107@users.noreply.github.com>
2025-03-05 10:18:53 +0900

    [Bronze III] Title: 신기한 수, Time: 2604 ms, Memory: 32412 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/17618. 신기한 수/신기한 수.py
+++ 백준/Bronze/17618. 신기한 수/신기한 수.py
@@ -1,5 +1,22 @@
-c = 0
-for i in range(1,int(input())+1):
-    if not i % sum(map(int,str(i))):
-        c += 1
-print(c)
+N = int(input())
+count = 0
+
+ds = 1
+if 1 % ds == 0:
+    count += 1
+
+for i in range(2, N + 1):
+    if (i - 1) % 10 != 9:
+        ds += 1
+    else:
+        k = 0
+        x = i - 1
+        while x % 10 == 9:
+            k += 1
+            x //= 10
+        ds = ds + 1 - 9 * k
+
+    if i % ds == 0:
+        count += 1
+
+print(count)
